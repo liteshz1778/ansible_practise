b90ddc4ae5f62ce685f1e4e07eaeb9240957de4a
= <=>
2026-03-20 11:47:21 -0400

    Added code changes reg. configuring target host in inventory file - 20/03/2026

--- modules/module_user/configure_host.sh
+++ modules/module_user/configure_host.sh
@@ -1,4 +1,6 @@
-inventory_json=$(ansible-inventory -i /root/playbooks/inventory --list)
+#!/bin/bash
+
+export inventory_json=$(ansible-inventory -i /root/playbooks/inventory --list)
 
 echo "inventory_json=$inventory_json"
 
@@ -7,3 +9,4 @@ export DB1_IP=$(echo "$inventory_json" | jq -r '._meta.hostvars.web2.ansible_hos
 
 echo "WEB1_IP=$WEB1_IP"
 echo "DB1_IP=$DB1_IP"
+
