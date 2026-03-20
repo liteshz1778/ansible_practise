a30abf6fd08b5b4a182518165c8a7b3b64630320
= <=>
2026-03-20 11:31:18 -0400

    Added code changes to modules/module_user/ - 20/03/2026

---
+++ modules/module_user/configure_host.sh
@@ -0,0 +1,9 @@
+inventory_json=$(ansible-inventory -i /root/playbooks/inventory --list)
+
+echo "inventory_json=$inventory_json"
+
+export WEB1_IP=$(echo "$inventory_json" | jq -r '._meta.hostvars.web1.ansible_host')
+export DB1_IP=$(echo "$inventory_json" | jq -r '._meta.hostvars.web2.ansible_host')
+
+echo "WEB1_IP=$WEB1_IP"
+echo "DB1_IP=$DB1_IP"
