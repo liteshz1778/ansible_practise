b7f336261d3cd73e3d5f110706a2810b74f2f941
= <=>
2026-03-23 15:19:53 -0400

    Added exp reg.Jinja templates related to accessing hostvars - 24/03/2026

---
+++ templates_exp/access_hostvars/templates/inventory.csv.js
@@ -0,0 +1,9 @@
+ipaddress, connection, port, user, appStatus, supportEmail
+{% for host in groups['all'] %}
+{{ hostvars[host].ansible_host }}, {{ hostvars[host].ansible_connection }}, {{ hostvars[host].ansible_port }}, {{ hostvars[host].ansible_user }}, {{ hostvars[host].appStatus }}, {{ hostvars[host].supportEmail }}
+{% endfor %}
+
+ipaddress, ansible_architecture, ansible_distribution_version, ansible_memtotal_mb, ansible_processor_cores, ansible_processor_count
+{% for host in groups['all'] %}
+{{ hostvars[host].ansible_host }}, {{ hostvars[host].ansible_architecture }}, {{ hostvars[host].ansible_distribution_version }}, {{ hostvars[host].ansible_memtotal_mb }}, {{ hostvars[host].ansible_processor_cores }}, {{ hostvars[host].ansible_processor_count }}
+{% endfor %}
